f05f5335cff2ccaa1d27b2b27b4b79ebfb773e5a
Manuella Filipe Peres <manuellafilipeperes@gmail.com>
2026-02-25 19:50:52 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -4,8 +4,42 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.connect("tcp://broker:5556")
 
-while True:
-    message = socket.recv()
-    print(f"Mensagem recebida: {message}", flush=True)
-    socket.send_string("World")
+tarefas = []
 
+while True:
+    mensagem = socket.recv_json()
+    comando = mensagem.get("comando")
+
+    if comando == "adicionar":
+        tarefa = mensagem.get("tarefa")
+        tarefas.append(tarefa)
+        socket.send_json({
+            "status": "ok",
+            "msg": "Tarefa adicionada!"
+        })
+
+    elif comando == "remover":
+        tarefa = mensagem.get("tarefa")
+        if tarefa in tarefas:
+            tarefas.remove(tarefa)
+            socket.send_json({
+                "status": "ok",
+                "msg": "Tarefa removida!"
+            })
+        else:
+            socket.send_json({
+                "status": "erro",
+                "msg": "Tarefa não encontrada."
+            })
+
+    elif comando == "listar":
+        socket.send_json({
+            "status": "ok",
+            "tarefas": tarefas
+        })
+
+    else:
+        socket.send_json({
+            "status": "erro",
+            "msg": "Comando inválido."
+        })
