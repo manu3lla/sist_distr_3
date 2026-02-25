7208314e403989e31ce3cba360b3b163d2ad28ea
Manuella Filipe Peres <manuellafilipeperes@gmail.com>
2026-02-25 19:51:06 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,15 +1,42 @@
 import zmq
-from time import sleep
+import json
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://broker:5555")
 
-i = 0
+def menu():
+    print("\n1 - Adicionar tarefa")
+    print("2 - Remover tarefa")
+    print("3 - Listar tarefas")
+    print("4 - Sair")
+
 while True:
-    print(f"Mensagem {i}:", end=" ", flush=True)
-    socket.send(b"Hello")
-    mensagem = socket.recv()
-    print(f"{mensagem}")
-    i += 1
-    sleep(0.5)
+    menu()
+    opcao = input("Escolha: ")
+
+    if opcao == "1":
+        tarefa = input("Digite a tarefa: ")
+        mensagem = {"comando": "adicionar", "tarefa": tarefa}
+        socket.send_json(mensagem)
+        resposta = socket.recv_json()
+        print(resposta["msg"])
+
+    elif opcao == "2":
+        tarefa = input("Digite a tarefa a remover: ")
+        mensagem = {"comando": "remover", "tarefa": tarefa}
+        socket.send_json(mensagem)
+        resposta = socket.recv_json()
+        print(resposta["msg"])
+
+    elif opcao == "3":
+        mensagem = {"comando": "listar"}
+        socket.send_json(mensagem)
+        resposta = socket.recv_json()
+
+        print("\nTarefas:")
+        for t in resposta["tarefas"]:
+            print("-", t)
+
+    elif opcao == "4":
+        break
